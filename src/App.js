248b4fa2f1fe9e3a141b3bde45850934949cbc8b
marivirden <97749369+marivirden@users.noreply.github.com>
2023-01-19 14:29:18 -0600

    Redoing first steps

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Search from "./search";
 
-import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
-      <h1>Homework Week 4</h1>
-      <Search />
-      <div>
-        <a href="https://github.com/marivirden/shecodes-plus-weather-app-react">
-          Open-source code
-        </a>
-        , by <a href="https://github.com/marivirden">Marianna Virden</a> from{" "}
-        <a href="https://www.shecodes.io/">SheCodes</a>
+      <div className="container">
+        <Search />
+        <footer>
+          <div>
+            <a href="https://github.com/marivirden/shecodes-plus-weather-app-react">
+              Open-source code
+            </a>
+            , by <a href="https://github.com/marivirden">Marianna Virden</a>{" "}
+            from <a href="https://www.shecodes.io/">SheCodes</a>
+          </div>
+        </footer>
       </div>
     </div>
   );
--- src/search.js
+++ src/search.js
@@ -1,54 +1,94 @@
 import React, { useState } from "react";
+import "./weather.css";
 import axios from "axios";
 
 export default function Search() {
-  const [city, setCity] = useState("");
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    getCityWeather();
-  }
-
-  function handleWeatherData(response) {
-    console.log(response.data);
-    setIsSubmitted(true);
-    setWeatherData(response.data);
-  }
-
-  function getCityWeather() {
-    const apiKey = "0c5cd3271ca5da27f88448575bac1056";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleWeatherData);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-
-    if (isSubmitted) {
-      setIsSubmitted(false);
-    }
-  }
-
   return (
-    <div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={updateCity} />
-          <input type="submit" value="search" />
-        </form>
-      </div>
-      {isSubmitted ? (
-        <div>
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a City.."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Austin</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="" />6 °F
+        </div>
+        <div className="col-6">
           <ul>
-            <li>Temperature: {Math.round(weatherData.main.temp)}°F </li>
-            <li>Description: {weatherData.weather[0].description}</li>
-            <li>Humidity: {Math.round(weatherData.main.humidity)}%</li>
-            <li>Wind: {Math.round(weatherData.wind.speed)} m/hr </li>
+            <li>Precipitation: 15%</li>
+            <li> Humidity: 75%</li>
+            <li>Wind: 13mp/hr</li>
           </ul>
         </div>
-      ) : null}
+      </div>
     </div>
   );
 }
+
+// export default function Search() {
+//   const [city, setCity] = useState("");
+//   const [isSubmitted, setIsSubmitted] = useState(false);
+//   const [weatherData, setWeatherData] = useState({});
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     getCityWeather();
+//   }
+
+//   function handleWeatherData(response) {
+//     console.log(response.data);
+//     setIsSubmitted(true);
+//     setWeatherData(response.data);
+//   }
+
+//   function getCityWeather() {
+//     const apiKey = "0c5cd3271ca5da27f88448575bac1056";
+//     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+//     axios.get(apiUrl).then(handleWeatherData);
+//   }
+
+//   function updateCity(event) {
+//     setCity(event.target.value);
+
+//     if (isSubmitted) {
+//       setIsSubmitted(false);
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <div>
+//         <form onSubmit={handleSubmit}>
+//           <input type="search" onChange={updateCity} />
+//           <input type="submit" value="search" />
+//         </form>
+//       </div>
+//       {isSubmitted ? (
+//         <div>
+//           <ul>
+//             <li>Temperature: {Math.round(weatherData.main.temp)}°F </li>
+//             <li>Description: {weatherData.weather[0].description}</li>
+//             <li>Humidity: {Math.round(weatherData.main.humidity)}%</li>
+//             <li>Wind: {Math.round(weatherData.wind.speed)} m/hr </li>
+//           </ul>
+//         </div>
+//       ) : null}
+//     </div>
+//   );
+// }
